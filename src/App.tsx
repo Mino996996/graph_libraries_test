4a96fce838c0a292272884ccede64e8a3a23f849
minoru akahori <minoruaka@gmail.com>
2022-09-22 16:31:50 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Plot from 'react-plotly.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3, 4, 10], y: [2, 5, 3, 20, 30]},
+        ]}
+        layout={ {width: 520, height: 340, title: 'A Fancy Plot'} }
+      />
     </div>
   );
 }
